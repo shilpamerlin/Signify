193aa5aaf98e3771d9d5d52e57b0e0a3ae3976eb
Shilpa Merlin joy <shilpamjoy@gmail.com>
2024-09-11 15:52:00 -0400

    Initial Commit

---
+++ Signify/SignifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignifyApp.swift
+//  Signify
+//
+//  Created by Shilpa Joy on 2024-09-11.
+//
+
+import SwiftUI
+
+@main
+struct SignifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
